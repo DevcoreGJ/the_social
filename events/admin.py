1e5d2d3bd0e78880703cf76dfb777cf4d5cfb067
Nic Ledger <nicledger@nigels-MacBook-Pro.local>
2022-11-17 16:34:08 +0000

    refactored admin.py file as it is best practice for further manipulation of the Venue table

--- events/admin.py
+++ events/admin.py
@@ -4,6 +4,14 @@ from .models import MyClubUser
 from .models import Event
 
 # Register your models here.
-admin.site.register(Venue)
+
+# Commenting out venue as adding a
+# more comprehensive feature that encapsulates
+
+#admin.site.register(Venue)
+
 admin.site.register(MyClubUser)
 admin.site.register(Event)
+
+# Better practice than the above original
+@admin.register(Venue)
